3ca496c5334faed97471670185371ad50e3a1478
unknown <johnsolojoseph@gmail.com>
2019-04-01 19:31:09 -0700

    first commit

---
+++ js/DB_CONFIG.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyCS9gg6lG45UmComTK0YIKBitZtPwCQ17k",
+    authDomain: "pet-hero-393ff.firebaseapp.com",
+    databaseURL: "https://pet-hero-393ff.firebaseio.com",
+    projectId: "pet-hero-393ff",
+    storageBucket: "pet-hero-393ff.appspot.com",
+    messagingSenderId: "117351241316"
+  };
